87a46aa317ffd42fad9766d21bfe5189674df663
Toke Frostholm Rasmussen <toke1011@edu.easj.dk>
2016-03-10 16:45:17 +0100

    Initial commit

---
+++ SQLQuery1.sql
@@ -0,0 +1,18 @@
+﻿-- Simple query
+SELECT * 
+FROM Customers;
+
+-- The DISTINCT keyword can be used to return only distinct (different) values.
+SELECT DISTINCT City 
+From Customers; 
+
+-- The WHERE clause is used to extract only those records that fulfill a specified criterion.
+Select *
+From Customers
+Where Country = 'Mexico';
+
+Select *
+From Customers
+Where CustomerID = 22;
+
+-- The AND & OR operators are used to filter records based on more than one condition.
